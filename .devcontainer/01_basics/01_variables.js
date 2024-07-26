4ec6378c16d7805e875da214a624fc3d269e565c
Arpitvasoya <143179220+Arpitvasoya@users.noreply.github.com>
2024-07-26 08:41:13 +0000

    container config

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144553
+let accountEmail = "arpit@google.com"
+var accountPassword = "12345"
+accountCity = "Surat"
+
+accountId = 2 
+
+console.log(acccountId);
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "arpit@google.com"
+var accountPassword = "12345"
+accountCity = "Surat"
+
+//accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "212121"
+accountCity = "ahmedabad"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
