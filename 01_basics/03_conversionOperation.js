6dbaac743c19c87d48d25ab641e17d7ee55e524f
Arpitvasoya <143179220+Arpitvasoya@users.noreply.github.com>
2024-07-26 17:47:09 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score =  "arpit"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abd" => NaN - not an number
+// "true" =>1, false =>0
+
+let isLoggedIn = "Arpit"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => false
+// "" => false
+// "Arpit" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
