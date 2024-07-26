70675e14b1878a80fbf62ef20c26ec3f13f60d79
Arpitvasoya <143179220+Arpitvasoya@users.noreply.github.com>
2024-07-26 17:18:48 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+//alert (3 + 3) // we are using nodejs, not brower
+
+console.log(3 + 3) // code readability should be high
+
+console.log("ARPIT")
+
+
+let name = "ARPIT"
+let age = 18
+let isloggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/flase
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+
+
+console.log(typeof null);
